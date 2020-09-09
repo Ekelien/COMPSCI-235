79077f53015c3051e7b2383e702070e9aac52a0f
Hongchen <ekelien.lin@gmail.com>
2020-09-09 12:32:45 +1200

    done

--- people_web_app/__init__.py
+++ people_web_app/__init__.py
@@ -6,6 +6,12 @@ from people_web_app.domain.model import Person
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object("config.Config")
+
+    with app.app_context():
+        from people_web_app.people_blueprint.people import people_blueprint
+        app.register_blueprint(people_blueprint)
+
 
     repo.repo_instance = repo.PeopleRepository(
         Person(74633, 'Julius', 'Caeser'),
--- people_web_app/people_blueprint/people.py
+++ people_web_app/people_blueprint/people.py
@@ -1,3 +1,4 @@
+
 from flask import Blueprint, render_template, url_for
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SubmitField
@@ -21,12 +22,41 @@ def home():
 
 @people_blueprint.route('/list')
 def list_people():
-    pass
+    return render_template(
+        "list_people.html",
+        find_person_url=url_for('people_bp.find_person'),
+        list_people_url=url_for('people_bp.list_people'),
+        people=repo.repo_instance
+    )
 
 
 @people_blueprint.route('/find', methods=['GET', 'POST'])
 def find_person():
-    pass
+    form=SearchForm()
+    if form.validate_on_submit():#Post
+        POSTedid=form.person_id.data
+        # for p in repo.repo_instance:
+        #     if POSTedid==p.id_number:
+        #         person=p
+        person=repo.repo_instance.get_person(POSTedid)
+        return render_template(
+                "list_person.html",
+                find_person_url=url_for('people_bp.find_person'),
+                list_people_url=url_for('people_bp.list_people'),
+                person=person)
+    return render_template(
+        "find_person.html",
+        find_person_url=url_for('people_bp.find_person'),
+        list_people_url=url_for('people_bp.list_people'),
+        handler_url=url_for("people_bp.find_person"),
+        form=form
+    )
 
+
+
+class SearchForm(FlaskForm):
+
+    person_id=IntegerField("Person id")
+    submit=SubmitField("Find")
 
 
